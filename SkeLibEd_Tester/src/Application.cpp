8c8162247d30790a95589fadc7fb68e4aae71d0f
wolfstrasz <boyan.yotov@abv.bg>
2018-10-06 15:53:51 +0100

    some chriono testing

--- SkeLibEd_Tester/src/Application.cpp
+++ SkeLibEd_Tester/src/Application.cpp
@@ -1,13 +1,41 @@
 #include <iostream>
 #include <limits.h>
 #include "skelibed.hpp"
-#include "normal_test.hpp"
+#include <chrono>
+//#include "normal_test.hpp"
 
 // MAIN
 int main()
 {
-	normal_main();
-	// -----------------------------------------------
-	//std::cin.get();
+	//normal_main()
+
+	// Get time of chrono call
+	// -----------------------
+	std::chrono::steady_clock::time_point chronoStart = std::chrono::steady_clock::now();
+	std::chrono::steady_clock::time_point chronoEnd = std::chrono::steady_clock::now();
+	auto chronoCallTime = chronoEnd - chronoStart;
+	std::cout << "chrono call = " << std::chrono::duration_cast<std::chrono::microseconds>(chronoCallTime).count() << std::endl;
+	std::cout << "chrono call = " << std::chrono::duration_cast<std::chrono::nanoseconds> (chronoCallTime).count() << std::endl;
+
+	// Calculate time of task
+	// ----------------------
+	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+	for (int i = 1; i < 1000000; i++) {
+		int a;
+	}
+
+	// Check time without precision
+	// ----------------------------
+	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << std::endl;
+	
+	// Remove chrono call time for better precision;
+	// ---------------------------------------------
+	end -= chronoCallTime;
+
+	std::cout << "Time difference precision = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
+	std::cout << "Time difference precision = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << std::endl;
+
+	std::cin.get();
 }
-/*dsadasd*/
