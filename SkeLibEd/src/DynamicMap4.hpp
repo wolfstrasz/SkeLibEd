81281ca8d3c31b2a0baaf741d6be56367cab4a8b
wolfstrasz <boyan.yotov@abv.bg>
2018-11-25 00:02:53 +0000

    fix

--- SkeLibEd/src/DynamicMap4.hpp
+++ SkeLibEd/src/DynamicMap4.hpp
@@ -64,7 +64,7 @@ public:
 			//double meanTime;
 			size_t startItems;
 			void switchWorkload(size_t newMeanWork, double workTime) {
-				std::cout << "SWITCH FROM: " << jobSize << "\t TO: "<< jobSize/ newMeanWork 
+				std::cout << "SWITCH FROM: " << jobSize << "\t TO: "<< (jobSize + newMeanWork) / 2
 					<< "\t CUZ: " << workTime << "\n";
 					this->jobSize = (this->jobSize + newMeanWork) / 2;
 					this->jobSize = this->jobSize == 0 ? 1 : jobSize;
